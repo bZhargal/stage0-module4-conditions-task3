c2b0441d6f57515ec8fe4d6d627c3766a1d0e5c3
zhargal <bzhargal@gmail.com>
2022-12-07 20:11:35 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (!String.valueOf(character).matches("[a-zA-Z]")) {
+            System.out.println("wrong alphabet!");
+        } else if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
     }
 }
